3c7856e1bd81e1d2f4597f5ce81f4b7c6504f92a
VirajCommits <virajmurab567@yahoo.com>
2025-01-11 13:33:14 -0700

    Fix gunicorn app entry point

--- backend/views.py
+++ backend/views.py
@@ -2,13 +2,13 @@
 
 from flask import redirect, request, jsonify, session
 from flask_socketio import send, join_room, leave_room, emit
-from socketio_instance import socketio  # Import the SocketIO instance
+from backend.socketio_instance import socketio  # Import the SocketIO instance
 from models import Message
 from extensions import db
 from oauthlib.oauth2 import WebApplicationClient
 import requests
 import os
-from models import Group,GroupMember, User, UserActivity
+from backend.models import Group,GroupMember, User, UserActivity
 from sqlalchemy.exc import SQLAlchemyError
 from datetime import datetime, date
 
