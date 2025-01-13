7e3eabb7387c91f1abc1f3794c61bc728e6f9c61
VirajCommits <virajmurab567@yahoo.com>
2025-01-13 14:46:50 -0700

    Connecting...

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,14 +3,20 @@
 import React from "react";
 
 export default function LoginPage() {
+  const handleLogin = () => {
+    window.location.href = "/api/google/login";
+
+  };
+
   return (
     <div style={styles.container}>
       <div style={styles.loginBox}>
-        <h1 style={styles.heading}>Lets get you signed in!</h1>
+        <h1 style={styles.heading}>Let’s get you signed in!</h1>
         <p style={styles.subheading}>Log in with your Google account to get started</p>
-        <a href="https://buddy-board-88fd54c902d8.herokuapp.com/api/google/login">
-          <button style={styles.loginButton}>Log In with Google</button>
-        </a>
+
+        <button style={styles.loginButton} onClick={handleLogin}>
+          Log In with Google
+        </button>
       </div>
     </div>
   );
@@ -22,7 +28,7 @@ const styles: { [key: string]: React.CSSProperties } = {
     display: "flex",
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "#1e1e2f", // Blueish-purple background
+    backgroundColor: "#1e1e2f",
     color: "#fff",
     fontFamily: "Arial, sans-serif",
   },
